cf89b6609638bcec069633f7937993ef70d8def9
arun3810 <125949906+arun3810@users.noreply.github.com>
2024-09-21 16:44:14 +0530

    details entered

--- src/Components/Details/index.jsx
+++ src/Components/Details/index.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import './style.scss'
-import { MailOutlined, LinkedinOutlined, GithubOutlined } from '@ant-design/icons';
+import { MailOutlined, LinkedinOutlined, GithubOutlined, WhatsAppOutlined, ProfileOutlined } from '@ant-design/icons';
 import { themeDecider } from '../CommonHelper';
+import ResumeFile from '../../Assets/Resume/Arun_Kumar_Gupta.pdf'
 
 const Details = () => {
     const contactMedia = [
         {
             id: 1,
             name: 'Github',
-            url: '',
-            icon: <MailOutlined />
+            url: 'https://github.com/arungupta3810',
+            icon: <GithubOutlined />
         },
         {
             id: 2,
             name: 'LinkedIn',
-            url: '',
+            url: 'https://www.linkedin.com/in/arun-gupta-148872194/',
             icon: <LinkedinOutlined />
         },
         {
-            id: 1,
+            id: 3,
             name: 'Email',
-            url: '',
-            icon: <GithubOutlined />
-        }
+            url: 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=DmwnWrRmTpGwGrxrDjKZQmssdpSDTGcKxqVPBPPNxcppdVvmLkRfFkGDnLxWpxNhZMJDdWVZtZBB',
+            icon: <MailOutlined />
+        },
+        {
+            id: 4,
+            name: 'Whatsapp',
+            url: 'https://api.whatsapp.com/send/?phone=%2B918652864081&text&type=phone_number&app_absent=0',
+            icon: <WhatsAppOutlined />
+        },
+        {
+            id: 5,
+            name: 'Resume',
+            url: ResumeFile,
+            icon: <ProfileOutlined />
+        },
     ]
     return (
         <div className='details'>
@@ -32,12 +45,12 @@ const Details = () => {
                 interest in Computer Science.</span></p>
             <span>🚀 Currently specializing in Frontend (React / Next.js)</span>
             <br />
-            <span>⚡Frontend Engineer at <span>Lighthouse Learning Private Limited</span></span>
+            <span>⚡Frontend Engineer at <span onClick={()=>window.open("https://www.lighthouse-learning.com/")}>Lighthouse Learning Private Limited</span></span>
             <div className='contact-details'>
                 {contactMedia?.map((media) =>
-                    <div className={`contact-media ${themeDecider()}`}>
+                    <a href={media?.url} target='_blank'><div className={`contact-media ${themeDecider()}`}>
                         <span>{media?.icon}{' '}{media?.name}</span>
-                    </div>)}
+                    </div></a>)}
             </div>
         </div>
     )
