84e71028bd610dd79583e7943a1482c5bffb8307
arun3810 <125949906+arun3810@users.noreply.github.com>
2024-09-27 01:01:39 +0530

    url updated mongo

--- backend/database/Config.js
+++ backend/database/Config.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/My_Portfolio');   //local db
-mongoose.connect("mongodb+srv://arungupta:Light%40run123@arun.9mkye.mongodb.net/");   //live db
+mongoose.connect("mongodb+srv://arungupta:Light%40run123@arun.9mkye.mongodb.net/My_Portfolio");   //live db
